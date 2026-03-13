df3dc9c55e13cb27fdeeb17587861db35d3406bc
ManhNguyen <57060858+nguyenmanhsonbg@users.noreply.github.com>
2026-03-13 13:10:03 +0700

    init

---
+++ src/main/java/com/g90/backend/G90BackendApplication.java
@@ -0,0 +1,13 @@
+package com.g90.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G90BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G90BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g90/backend/G90BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g90.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G90BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
